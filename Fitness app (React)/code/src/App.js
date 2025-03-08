3f85f0b86c26e75027848fa373f8428775a59778
nakshatra-s-p <nakshatranachu6@gmail.com>
2025-03-08 17:49:39 +0530

    ADD

--- Fitness app (React)/code/src/App.js
+++ Fitness app (React)/code/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       <Navbar />
       
-      <Routes>
+      <Routes >
 
         <Route path="/" element={<Home />} />
         <Route path="/bodyPart/:id" element={<BodyPartsCategory />} />
